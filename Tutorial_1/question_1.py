c4dd8c78aa19d3a1eaab669108e595fe8468881f
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-09-18 15:59:45 -0600

    Files added

--- Tutorial_1/question_1.py
+++ Tutorial_1/question_1.py
@@ -27,5 +27,6 @@ data_sample.sort()
 list_len = len(data_sample)
 mean = list_stats(data_sample)/list_len
 iqr = pth_percent(data_sample, list_len, 75)-pth_percent(data_sample, list_len, 25)
+test = int(input("Please enter an integer"))
 
 print(f'{pth_percent(data_sample, list_len, 75)}')
